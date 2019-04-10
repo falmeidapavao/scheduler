ebd7ed3461fdac10c8a556a8de10bca10d95d7cf
Fred Almeida <falmeidapavao@gmail.com>
2019-04-10 22:41:28 +0000

    Calendar enhancement
    - Styled calendar;
    - Added select function.

--- app/calendar/calendar.js
+++ app/calendar/calendar.js
@@ -3,44 +3,57 @@
 angular.module('app').component('calendar', {
   templateUrl: 'calendar/calendar.html',
   bindings: {
-    calendar: '='
+    events: '='
   },
-  controller: function () {
-    this.calendar = [];
-    this.populate(2019, 1);
-
-    /**
-     * Populates calendar based on year and month. 
-     */
-    this.populate = function (year, month) {
-      var firstDay = new Date(year, month, 1);
-      var lastDay = new Date(year, month + 1, 0);
-      /* Get day in which calendar starts(can be day from previous month)*/
-      var start = new Date(
-        firstDay.setDate(firstDay.getDate() - firstDay.getDay())
-      );
-      /* Get day in which calendar ends(can be day from next month)*/
-      var end = new Date(
-        lastDay.setDate(lastDay.getDate() + (DAYS_PER_WEEK - (lastDay.getDay() + 1)))
-      );
-      /* Populate calendar */
-      while (start < end) {
-        var week = [];
-
-        for (var i = 0; i < DAYS_PER_WEEK; i++) {
-          var clone = new Date(start);
-          week.push(clone);
-          start.setDate(start.getDate() + 1);
-        }
-        this.calendar.push(week);
-      }
-    }
-
-    /**
-     * Clears calendar.
-     */
-    this.clear = function () {
-      this.calendar = [];
-    }
-  }
+  controller: CalendarController
 });
+
+function CalendarController() {
+  var ctrl = this;
+  ctrl.calendar = [];
+  ctrl.$onInit = function() {
+    ctrl.populate(2019, 5);
+  };
+
+  /**
+   * Populates calendar based on year and month. 
+   */
+  ctrl.populate = function (year, month) {
+    const DAYS_PER_WEEK = 7;
+    var firstDay = new Date(year, month, 1);
+    var lastDay = new Date(year, month + 1, 0);
+    /* Get day in which calendar starts(can be day from previous month)*/
+    var start = new Date(
+      firstDay.setDate(firstDay.getDate() - firstDay.getDay())
+    );
+    /* Get day in which calendar ends(can be day from next month)*/
+    var end = new Date(
+      lastDay.setDate(lastDay.getDate() + (DAYS_PER_WEEK - (lastDay.getDay() + 1)))
+    );
+    /* Populate calendar */
+    while (start < end) {
+      var week = [];
+
+      for (var i = 0; i < DAYS_PER_WEEK; i++) {
+        var clone = new Date(start);
+        week.push(clone);
+        start.setDate(start.getDate() + 1);
+      }
+      ctrl.calendar.push(week);
+    }
+  };
+
+  /**
+   * Clears calendar.
+   */
+  ctrl.clear = function () {
+    ctrl.calendar = [];
+  };
+
+  /**
+   * Selects a date.
+   */
+   ctrl.select = function(date) {
+    date.selected = !date.selected;
+   };
+}
--- app/home/home.js
+++ app/home/home.js
@@ -10,5 +10,12 @@ angular.module('app.home', ['ngRoute'])
 }])
 
 .controller('HomeCtrl', [function() {
-  
+  /*let eventModel = {
+    date: 'date in milliseconds',
+    eventType: ''
+  };*/
+  // Events as eventModel hash table
+  // Key is date in milliseconds, value is aditional event properties.
+  var ctrl = this;
+  ctrl.events = [];
 }]);
