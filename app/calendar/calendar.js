6dc437183270a63efb2d9060020b2bfef378d69a
Fred Almeida <falmeidapavao@gmail.com>
2019-04-11 16:05:50 +0000

    Added calendar month change feature. Minor refactors.

--- app/calendar/calendar.js
+++ app/calendar/calendar.js
@@ -3,57 +3,66 @@
 angular.module('app').component('calendar', {
   templateUrl: 'calendar/calendar.html',
   bindings: {
-    events: '='
+    events: '=',
+    year: '<',
+    month: '<'
   },
   controller: CalendarController
 });
 
 function CalendarController() {
   var ctrl = this;
-  ctrl.calendar = [];
-  ctrl.$onInit = function() {
-    ctrl.populate(2019, 5);
+
+  // When calendar inits.
+  ctrl.$onInit = () => {
+    ctrl.calendar = [];
+    ctrl.calendar = ctrl.populate(ctrl.year, ctrl.month);
   };
 
-  /**
-   * Populates calendar based on year and month. 
-   */
-  ctrl.populate = function (year, month) {
+  // Populates calendar based on year and month.
+  ctrl.populate = (year, month) => {
+    let calendar = [];
+    let firstDay = new Date(year, month, 1);
+    let lastDay = new Date(year, month + 1, 0);
     const DAYS_PER_WEEK = 7;
-    var firstDay = new Date(year, month, 1);
-    var lastDay = new Date(year, month + 1, 0);
-    /* Get day in which calendar starts(can be day from previous month)*/
-    var start = new Date(
+
+    // Get day in which calendar starts(can be day from previous month)
+    let start = new Date(
       firstDay.setDate(firstDay.getDate() - firstDay.getDay())
     );
-    /* Get day in which calendar ends(can be day from next month)*/
-    var end = new Date(
+
+    // Get day in which calendar ends(can be day from next month)
+    let end = new Date(
       lastDay.setDate(lastDay.getDate() + (DAYS_PER_WEEK - (lastDay.getDay() + 1)))
     );
-    /* Populate calendar */
-    while (start < end) {
-      var week = [];
 
-      for (var i = 0; i < DAYS_PER_WEEK; i++) {
-        var clone = new Date(start);
+    // Populate calendar.
+    while (start < end) {
+      let week = [];
+
+      for (let i = 0; i < DAYS_PER_WEEK; i++) {
+        let clone = new Date(start);
         week.push(clone);
         start.setDate(start.getDate() + 1);
       }
-      ctrl.calendar.push(week);
+      calendar.push(week);
     }
+    return calendar;
   };
 
-  /**
-   * Clears calendar.
-   */
-  ctrl.clear = function () {
-    ctrl.calendar = [];
-  };
-
-  /**
-   * Selects a date.
-   */
-   ctrl.select = function(date) {
+  // Selects a date.
+  ctrl.select = (date) => {
     date.selected = !date.selected;
-   };
+  };
+
+  // Month change.
+  ctrl.changeMonth = (value) => {
+    let diff = value - ctrl.month;
+    let currentDate = new Date(ctrl.year, ctrl.month);
+    currentDate.setMonth(currentDate.getMonth() + diff);
+    ctrl.year = currentDate.getFullYear();
+    ctrl.month = currentDate.getMonth();
+    ctrl.calendar = [];
+    ctrl.calendar = ctrl.populate(ctrl.year, ctrl.month);
+  };
 }
--- app/home/home.js
+++ app/home/home.js
@@ -2,20 +2,29 @@
 
 angular.module('app.home', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/home', {
-    templateUrl: 'home/home.html',
-    controller: 'HomeCtrl'
-  });
-}])
+  .config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/home', {
+      templateUrl: 'home/home.html',
+      controller: 'HomeCtrl'
+    });
+  }])
 
-.controller('HomeCtrl', [function() {
-  /*let eventModel = {
-    date: 'date in milliseconds',
-    eventType: ''
-  };*/
-  // Events as eventModel hash table
-  // Key is date in milliseconds, value is aditional event properties.
-  var ctrl = this;
-  ctrl.events = [];
-}]);
+  .controller('HomeCtrl', ['$http', function ($http) {
+    var ctrl = this;
+    let today = new Date();
+
+    // Data for calendar.
+    ctrl.events = [];
+    ctrl.calendarStartYear = today.getFullYear();
+    ctrl.calendarStartMonth = today.getMonth();
+
+    // Get events.
+    $http({
+      method: 'GET',
+      url: 'mocks/events.json'
+    }).then(function (response) {
+      if(response.data) ctrl.events = response;
+      else console.error(response);
+    });
+  }
+]);
